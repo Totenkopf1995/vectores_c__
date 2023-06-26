82314b557f5546857f1f2343a3d84d368e876213
Totenkopf1995 <121690654+Totenkopf1995@users.noreply.github.com>
2023-06-25 21:50:18 -0500

    Update main.cpp

--- vectores/main.cpp
+++ vectores/main.cpp
@@ -6,25 +6,36 @@ int main() {
 
     int cantidadVector;
 
+    cout << endl;
+
     cout << "Cuantos elemtos tendra el vector: " << endl;
     cin >> cantidadVector;
 
+    cout << endl;
+
     vector<int> records(cantidadVector);
 
+    cout << "---------------------------------------------" << endl;
     cout << "escribe los " << cantidadVector << " numeros" << endl;
+    cout << "---------------------------------------------" << endl;
 
+    cout << endl;
 
     for (int i = 0; i < records.size(); ++i) {
 
+        cout << "indica el elemento " << i+1 << endl;
         cin >> records[i];
 
     }
 
+    cout << endl;
+    cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
     cout << "Los numeros ingresados son: " << endl;
     for (int j = 0; j < records.size(); ++j) {
 
         cout << records[j] << " ";
     }
+    cout << "\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
 
     cout << endl;
 
